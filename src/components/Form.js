869b87b24ada2e09e76e963c5b92823c02ad60d8
TahaBoulehmi <contact@btaha.com>
2019-03-14 13:07:50 +0100

    updating the bootstrap version and showing an alert if data not found

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import { Form  as BootstrapForm} from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 import config from './../config/config.js'; //contains the api url
 
 
 function Form() {
   const [websiteUrl, setWebsiteUrl] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [foundData, setFoundData] = useState(false);
 
   function fetchData(websiteUrl) { //Fetching the data
+    setFormSubmitted(true);
 
   }
 
@@ -23,6 +27,13 @@ function Form() {
       <Button onClick={() => fetchData(websiteUrl)} variant="primary" type="submit">
         Submit
       </Button>
+      {(websiteUrl !== '' && !foundData && formSubmitted) ?
+      <Alert dismissible variant="danger">
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          Data not found for this website url!
+        </p>
+      </Alert> : null}
     </BootstrapForm>
   );
 }
