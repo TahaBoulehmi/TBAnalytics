ac92002bcff7c93e8b0bec72af6369b6f10e039b
TahaBoulehmi <contact@btaha.com>
2019-03-14 20:44:26 +0100

    pushing the context file

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CONFIG from './../config/Config.js';
 
-//Creating  a global context
+//Creating a global context
 const DataContext = React.createContext(CONFIG.CONTEXT);
 
 export default DataContext;
