d74dd3d76489901e46d6fd8918a910a90d0f96db
TahaBoulehmi <contact@btaha.com>
2019-03-14 12:09:58 +0100

    showing the right component dynamtically

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './App.css';
+
+//importing components
+import Form from './components/Form.js';
+import Analytics from './components/Form.js';
+
+//importing config files
 import data from './context/context.js';
 import config from './config/config.js';
 
@@ -7,13 +13,19 @@ const DataContext = React.createContext(data);
 const ConfigContext = React.createContext(config);
 
 class App extends React.Component {
+  constructor(props) {
+   super(props);
+   this.state = data;
+  }
+
   render() {
     // In this example, we're passing "dark" as the current value.
+    const output = this.state.SiteName? <Analytics /> : <Form /> ;
     return (
       <DataContext.Provider value={data}>
         <ConfigContext.Provider value={config}>
           <p>
-            Hello World
+            {output}
           </p>
         </ConfigContext.Provider>
       </DataContext.Provider>
