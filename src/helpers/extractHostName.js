3575f1b39d24e58a19271ee31a7ce55417c5b87e
TahaBoulehmi <contact@btaha.com>
2019-03-15 15:19:12 +0100

    converting url link to lowercase

--- src/helpers/extractHostName.js
+++ src/helpers/extractHostName.js
@@ -14,7 +14,7 @@ const extractHostname = function (url) {
   //find & remove "?"
   hostname = hostname.split('?')[0];
 
-  return hostname.replace("www.");
+  return hostname.toLowerCase().replace("www.", "");
 }
 
 export default extractHostname;
