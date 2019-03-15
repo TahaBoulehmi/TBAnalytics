c859c35dff957a0ed91c5ffac80d854d3c20fc8e
TahaBoulehmi <contact@btaha.com>
2019-03-15 01:53:38 +0100

    adding D3JS Graph

--- src/components/analytics/TopOrganicKeywords.js
+++ src/components/analytics/TopOrganicKeywords.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React,  { useEffect } from 'react'
 import './TopOrganicKeywords.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import pieChart from 'helpers/pieChart';
 
 function TopOrganicKeywords(props) {
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Adding the D3Js graph using the browser's api
+    pieChart();
+  });
 
   return (
-    <div>
-    TopOrganicKeywords
-    </div>
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col>
+          <h3>Top Organic Keywords</h3>
+          <div id="TopOrganicKeywords"></div>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 }
 
